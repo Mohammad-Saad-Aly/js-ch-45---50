9f7588dd8c1bde2913a9685a2d61c8595ee01fc5
Mohammad Saad Ali <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2021-04-26 15:56:05 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+// function foo(a,b){
+//     return a + b
+// }
+
+// function foo(){
+//     alert('Button Clicked')
+// }
+
+// function foo(){
+//     var acha = document.getElementById('fff')
+//     alert(acha.value)
+//     acha.value = ""
+// }
+
+var global = prompt("enter kardo kuch bhi") 
+
+function chalana(){
+
+    var acha = document.getElementById('ccc').innerHTML = global
+}
